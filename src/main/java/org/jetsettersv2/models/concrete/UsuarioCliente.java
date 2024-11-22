5bdd1d1e725e15cc3ee1a3347a8a833f3d787e49
rebekaa333 <rebeca.camargo.natalia@gmail.com>
2024-11-22 18:37:21 -0300

    comiit6

--- src/main/java/org/jetsettersv2/models/concrete/UsuarioCliente.java
+++ src/main/java/org/jetsettersv2/models/concrete/UsuarioCliente.java
@@ -1,9 +1,13 @@
 package org.jetsettersv2.models.concrete;
 
+import org.jetsettersv2.collections.ArrayListGeneric;
 import org.jetsettersv2.models.abstracts.Persona;
+
+import java.util.ArrayList;
 
 public class UsuarioCliente extends Persona {
 
+    private ArrayListGeneric<Reserva> misReservas;
 
     public UsuarioCliente() {
         super();
@@ -13,15 +17,22 @@ public class UsuarioCliente extends Persona {
         super.imprimir();
     }
 
-//    public Reserva hacerReserva(){
-//        Reserva reserva = new Reserva();
-//    }
+    public ArrayListGeneric<Reserva> getMisReservas() {
+        return misReservas;
+    }
+
+    public UsuarioCliente misReservas(ArrayListGeneric<Reserva> misReservas) {
+        this.misReservas = misReservas;
+        return this;
+    }
+
+    public  Reserva hacerReserva(){
+        Reserva reserva = new Reserva();
+        System.out.println("Reserva realizada");
+        return reserva;
+    }
 
 
-//    +verPerfil(); / modificarDatos();
-//+hacerReserva();
-//+registrarEquipaje() : Equipaje;
-//+consultarEstadoReserva();
 //+RealizarCheckIn() : Pasajero;
 //+RegistrarEquipaje() : Equipaje
 }
