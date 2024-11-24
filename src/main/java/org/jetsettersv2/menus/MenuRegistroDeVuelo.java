47fbf0fbc4eec997d5e32d1a1909f39d96d1b60b
CamilaSetacosky <camilasetacosky55@gmail.com>
2024-11-24 20:44:40 -0300

    commitSubMenuVuelo

--- src/main/java/org/jetsettersv2/menus/MenuRegistroDeVuelo.java
+++ src/main/java/org/jetsettersv2/menus/MenuRegistroDeVuelo.java
@@ -6,13 +6,10 @@ import org.jetsettersv2.models.concrete.*;
 import java.util.Scanner;
 
 public class MenuRegistroDeVuelo {
-    public static void main(String[] args) {
+    public static void subMenuVuelo() {
         Scanner scanner = new Scanner(System.in);
         RegistroDeVuelo registroDeVuelo = new RegistroDeVuelo();
 
-        //Ver como relaciionarlo con el archivo
-        //Hacer metodo realizar cheeckin
-        // agregar estado de checkin COMPLETADO Y NO_REALIZADO
 
         // Configurar un avión para el registro
         Avion avion = new Avion(); // inicializar el avión con capacidad y otros detalles.
@@ -26,11 +23,9 @@ public class MenuRegistroDeVuelo {
 
         do {
             System.out.println("'\n' Menú registro de vuelo: ");
-            System.out.println("1. Agregar pasajero");
-            System.out.println("2. Agregar equipaje");
-            System.out.println("3. Agregar tripulante de cabina");
-            System.out.println("4. Agregar tripulante técnico");
-            System.out.println("5. Salir");
+            System.out.println("2. Agregar tripulante de cabina");
+            System.out.println("3. Agregar tripulante técnico");
+            System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
 
             opcion = scanner.nextInt();
@@ -39,36 +34,20 @@ public class MenuRegistroDeVuelo {
             try {
                 switch (opcion) {
                     case 1:
-                        System.out.print("Ingrese el nombre del pasajero: ");
-                        String nombrePasajero = scanner.nextLine();
-                        Pasajero pasajero = new Pasajero(new UsuarioCliente(nombrePasajero)); // Crear pasajero
-                        //validar que exista en el archivo, lo hago desp. fijarme si hace falta
-                        pasajero.getCheckIn().setEstadoCheck(EstadoCheck.COMPLETAD0); // Simular check-in completado
-                        registroDeVuelo.agregarPasajero(pasajero);
-                        System.out.println("Pasajero agregado exitosamente.");
-                        break;
-                    case 2:
-                        System.out.print("Ingrese el peso del equipaje: ");
-                        double pesoEquipaje = scanner.nextDouble();
-                        Equipaje equipaje = new Equipaje(pesoEquipaje); // Crear equipaje
-                        registroDeVuelo.agregarEquipaje(equipaje);
-                        System.out.println("Equipaje agregado exitosamente.");
-                        break;
-                    case 3:
                         System.out.print("Ingrese el nombre del tripulante de cabina: ");
                         String nombreCabina = scanner.nextLine();
                         TripulacionCabina tripulanteCabina = new TripulacionCabina(nombreCabina); // Crear tripulante
                         registroDeVuelo.agregarTripulanteCabina(tripulanteCabina);
                         System.out.println("Tripulante de cabina agregado exitosamente.");
                         break;
-                    case 4:
+                   /* case 2:
                         System.out.print("Ingrese el nombre del tripulante técnico: ");
                         String nombreTecnico = scanner.nextLine();
                         TripulacionTecnica tripulanteTecnico = new TripulacionTecnica(nombreTecnico); // Crear tripulante técnico
                         registroDeVuelo.agregarTripulanteTecnico(tripulanteTecnico);
                         System.out.println("Tripulante técnico agregado exitosamente.");
-                        break;
-                    case 5:
+                        break;*/
+                    case 0:
                         System.out.println("Saliendo del sistema...");
                         break;
                     default:
